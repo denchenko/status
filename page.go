5c1e8047e6c3a85ca36a67888d4d902434b3f478
Denis Shevchenko <denis.shevchenko@outlook.com>
2025-05-25 18:01:22 +0300

    page: configure via options

--- page.go
+++ page.go
@@ -8,45 +8,89 @@ import (
 	"runtime/debug"
 )
 
-//go:embed page.tmpl
-var tpl []byte
+var (
+	//go:embed page.tmpl
+	defaultTemplateContent string
+	defaultTemplate        = template.Must(template.New("page").Parse(defaultTemplateContent))
+)
 
+// Page represents a status page that can be served via HTTP
 type Page struct {
-	tmpl *template.Template
-	hc   *HealthChecker
-	urls []navURL
+	title string
+	tmpl  *template.Template
+	hc    *HealthChecker
+	links []Link
 }
 
-func NewPage() (*Page, error) {
-	tmpl, err := template.New("page").Parse(string(tpl))
-	if err != nil {
-		return nil, fmt.Errorf("parsing html template: %w", err)
+// PageOption is a function that configures a Page
+type PageOption func(*Page)
+
+// WithTitle sets the title of the status page
+func WithTitle(title string) PageOption {
+	return func(p *Page) {
+		p.title = title
 	}
-	return &Page{
-		tmpl: tmpl,
-	}, nil
 }
 
-func (p *Page) WithHealthChecker(hc *HealthChecker) {
-	p.hc = hc
+// WithTemplate sets a custom HTML template for the status page
+func WithTemplate(tmpl *template.Template) PageOption {
+	return func(p *Page) {
+		p.tmpl = tmpl
+	}
 }
 
-type navURL struct {
+// WithHealthChecker sets the health checker for the status page
+func WithHealthChecker(hc *HealthChecker) PageOption {
+	return func(p *Page) {
+		p.hc = hc
+	}
+}
+
+// WithLink adds a navigation link to the status page
+func WithLink(name, url string) PageOption {
+	return func(p *Page) {
+		p.links = append(p.links, Link{
+			Name: name,
+			URL:  url,
+		})
+	}
+}
+
+// NewPage creates a new status page with the given options
+func NewPage(opts ...PageOption) *Page {
+	p := &Page{
+		title: "System Status",
+		tmpl:  defaultTemplate,
+	}
+
+	for _, opt := range opts {
+		opt(p)
+	}
+
+	return p
+}
+
+// Link represents a navigation link in the status page
+type Link struct {
 	Name string
 	URL  string
 }
 
-func (p *Page) WithURL(name, url string) {
-	p.urls = append(p.urls, navURL{
-		Name: name,
-		URL:  url,
-	})
+// PageData contains the data that will be rendered in the status page template
+type PageData struct {
+	Title         string
+	Version       string
+	Revision      string
+	CommitDate    string
+	HealthResults []HealthCheckResult
+	Links         []Link
 }
 
+// Handler returns an HTTP handler that serves the status page
 func (p *Page) Handler() http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		version, revision, commitDate := retrieveBuildInfo()
+	version, revision, commitDate := retrieveBuildInfo()
 
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var healthResults []HealthCheckResult
 		if p.hc != nil {
 			var err error
@@ -57,29 +101,18 @@ func (p *Page) Handler() http.HandlerFunc {
 			}
 		}
 
-		data := struct {
-			Title         string
-			Heading       string
-			Message       string
-			Version       string
-			Revision      string
-			CommitDate    string
-			HealthResults []HealthCheckResult
-			URLs          []navURL
-		}{
-			Title:         "System Status",
-			Heading:       "System Status Dashboard",
-			Message:       "Current status of all system components and dependencies.",
+		data := PageData{
+			Title:         p.title,
 			Version:       version,
 			Revision:      revision,
 			CommitDate:    commitDate,
 			HealthResults: healthResults,
-			URLs:          p.urls,
+			Links:         p.links,
 		}
 
 		w.Header().Set("Content-Type", "text/html")
 		if err := p.tmpl.Execute(w, data); err != nil {
-			http.Error(w, "Error executing template", http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("Error executing template :%v", err), http.StatusInternalServerError)
 		}
 	})
 }
